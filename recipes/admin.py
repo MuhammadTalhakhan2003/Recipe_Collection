35c2a7a3e78972315c78c79079e2b8613f33cd9c
MuhammadTalhaKhan2003 <talhakhan050203@gmail.com>
2024-10-13 23:16:51 +0500

    Second Commit in Settings  Django RecipeCollection

--- recipes/admin.py
+++ recipes/admin.py
@@ -1,7 +1,4 @@
 from django.contrib import admin
 from .models import Recipe
 
-class RecipeAdmin(admin.ModelAdmin):
-    list_display = ('title', 'ingredients', 'created_at')  # Ensure you only include fields that exist
-
-admin.site.register(Recipe, RecipeAdmin)
+admin.site.register(Recipe)
